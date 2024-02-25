a1040c839ddfe99ac15a0cc9a3742f309112a401
Fred Lee <juhee.fred.lee@gmail.com>
2024-02-25 12:52:07 +0900

    chore: 파일 경로 수정

---
+++ juhee/src/inf/string/Q03.java
@@ -0,0 +1,20 @@
+package inf.string;
+
+import java.io.*;
+
+/**
+ * 문장 속 단어
+ */
+public class Q03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter stdOut = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] arr = stdIn.readLine().split(" ");
+        String longest = "";
+        for (String x : arr) if (x.length() > longest.length()) longest = x;
+        stdOut.write(longest);
+        stdOut.flush();
+        stdOut.close();
+        stdIn.close();
+    }
+}
