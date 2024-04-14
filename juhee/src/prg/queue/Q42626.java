fd880f2d340648e9e17295b4851a664c4da69e79
Fred Lee <juhee.fred.lee@gmail.com>
2024-04-14 19:37:45 +0900

    #17 [PRG] 더 맵게 / hard / 40 min
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

--- juhee/src/prg/queue/Q42626.java
+++ juhee/src/prg/queue/Q42626.java
@@ -8,12 +8,13 @@ import java.util.PriorityQueue;
 public class Q42626 {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Integer> que = new PriorityQueue<>();
+        PriorityQueue<Integer> que = new PriorityQueue<>(); // heap으로 구성된 priority queue 사용
 
         for (int i = 0; i < scoville.length; i++) {
             que.offer(scoville[i]);
         }
-        while(que.peek() < K) {
+
+        while(que.peek() < K) { // Priority Queue는 오름차순으로 요소를 반환함.
             if (que.size() == 1)
                 return -1;
             que.offer(que.poll() + que.poll() * 2);
