19ff9811183f741eafdc151742860dbd2992f2ad
Fred Lee <juhee.fred.lee@gmail.com>
2024-05-07 18:06:23 +0900

    #24 [INF] 이진트리 순회(깊이우선탐색) / easy / 10 min
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

---
+++ juhee/src/inf/graph/Node.java
@@ -0,0 +1,13 @@
+package inf.graph;
+
+public class Node {
+    int data;
+    Node lt;
+    Node rt;
+
+    public Node(int data) {
+        this.data = data;
+        this.lt = null;
+        this.rt = null;
+    }
+}
