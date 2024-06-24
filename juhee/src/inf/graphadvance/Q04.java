d1c14200e0e03bc7dfaf29a22c265c8a4756ce1f
Fred Lee <juhee.fred.lee@gmail.com>
2024-06-24 22:53:08 +0900

    #39 [INF] 송아지 찾기(BFS) / easy/ 19min
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

---
+++ juhee/src/inf/graphadvance/Q04.java
@@ -0,0 +1,48 @@
+package inf.graphadvance;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 4. 송아지 찾기(BFS)
+ */
+public class Q04 {
+    static int[] ch = new int[100001], steps = new int[] {-1, 1, 5};
+    public static void main(String[] args) throws IOException {
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter stdOut = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(stdIn.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        stdOut.write(String.valueOf(findMyCalf(S, E)));
+        stdOut.flush();
+        stdOut.close();
+        stdIn.close();
+    }
+
+    static int findMyCalf(int S, int E) {
+        int cnt = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(S);
+        ch[S] = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cq = q.poll();
+                if (cq == E) return cnt;
+                for (int step : steps) {
+                    int next = cq + step;
+                    if (next < 0 || next > 10000) continue;
+                    if (ch[next] == 0) {
+                        q.offer(next);
+                        ch[next] = 1;
+                    }
+                }
+            }
+            cnt++;
+        }
+        return -1;
+    }
+}
